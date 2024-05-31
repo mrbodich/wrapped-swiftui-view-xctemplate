d76c8ae408843c58729891ab0a4c1dbb65b4b790
Bogdan Chornobryvets <ternopost@gmail.com>
2024-05-31 09:26:56 -0400

    Initial commit

---
+++ WrappedSwiftUI View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,30 @@
+//___FILEHEADER___
+
+import Foundation
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+    
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#if DEBUG
+struct ___FILEBASENAMEASIDENTIFIER____PreviewWrapper: View {
+    
+    
+    var body: some View {
+        ___FILEBASENAMEASIDENTIFIER___(
+            
+        )
+    }
+}
+
+struct ___FILEBASENAMEASIDENTIFIER____Preview: PreviewProvider {
+    static var previews: some View {
+        ___FILEBASENAMEASIDENTIFIER____PreviewWrapper()
+    }
+}
+#endif
